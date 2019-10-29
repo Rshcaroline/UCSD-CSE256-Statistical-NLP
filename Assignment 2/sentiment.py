14d4ba16086791f4c5a2850160020fcd0db82ec3
RshCaroline <RshCaroline@163.com>
2019-10-28 23:58:49 -0700

    update report

--- Assignment 2/sentiment.py
+++ Assignment 2/sentiment.py
@@ -2,7 +2,7 @@
 @Author: 
 @Date: 2019-03-29 11:00:03
 @LastEditors: Shihan Ran
-@LastEditTime: 2019-10-27 18:20:32
+@LastEditTime: 2019-10-28 21:44:53
 @Email: rshcaroline@gmail.com
 @Software: VSCode
 @License: Copyright(C), UCSD
@@ -14,6 +14,7 @@
 import numpy as np
 import tarfile
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
@@ -24,14 +25,43 @@ import classify
 
 
 def data_analysis(sentiment):
+    # Number of characters
     pos_len = [len(s) for i, s in enumerate(sentiment.train_data) if sentiment.train_labels[i] == 'POSITIVE' ]
     neg_len = [len(s) for i, s in enumerate(sentiment.train_data) if sentiment.train_labels[i] == 'NEGATIVE']
-    pos = go.Box(y=pos_len, name = 'Positive Reviews', boxmean=True)
-    neg = go.Box(y=neg_len, name = 'Negative Reviews', boxmean=True)
-    data = [pos, neg]
-    layout = go.Layout(title = "Average Length of Positive vs. Negative")
-    fig = go.Figure(data=data, layout=layout)
-    fig.show()
+    plt.hist(pos_len, rwidth=0.9, align='mid', alpha=0.8, label='Positive Reviews')
+    plt.hist(neg_len, rwidth=0.9, align='mid', alpha=0.8, label='Negative Reviews')
+    plt.title("Number of Characters")
+    plt.legend()
+    plt.show()
+
+    # Number of words
+    pos_len = [len(s.split(" ")) for i, s in enumerate(sentiment.train_data) if sentiment.train_labels[i] == 'POSITIVE' ]
+    neg_len = [len(s.split(" ")) for i, s in enumerate(sentiment.train_data) if sentiment.train_labels[i] == 'NEGATIVE']
+    plt.hist(pos_len, rwidth=0.9, align='mid', alpha=0.8, label='Positive Reviews')
+    plt.hist(neg_len, rwidth=0.9, align='mid', alpha=0.8, label='Negative Reviews')
+    plt.title("Number of Words")
+    plt.legend()
+    plt.show()
+
+    # Average Word Length
+    pos_len = [sum(len(word) for word in s.split(" "))/len(s.split(" ")) for i, s in enumerate(sentiment.train_data) if sentiment.train_labels[i] == 'POSITIVE' ]
+    neg_len = [sum(len(word) for word in s.split(" "))/len(s.split(" ")) for i, s in enumerate(sentiment.train_data) if sentiment.train_labels[i] == 'NEGATIVE']
+    plt.hist(pos_len, rwidth=0.9, align='mid', bins=range(2,8), alpha=0.8, label='Positive Reviews')
+    plt.hist(neg_len, rwidth=0.9, align='mid', bins=range(2,8), alpha=0.8, label='Negative Reviews')
+    plt.title("Average Word Length")
+    plt.legend()
+    plt.show()
+
+    # Number of Stopwords
+    from nltk.corpus import stopwords
+    stop = stopwords.words('english')
+    pos_len = [len([x for x in s.split() if x in stop]) for i, s in enumerate(sentiment.train_data) if sentiment.train_labels[i] == 'POSITIVE' ]
+    neg_len = [len([x for x in s.split() if x in stop]) for i, s in enumerate(sentiment.train_data) if sentiment.train_labels[i] == 'NEGATIVE']
+    plt.hist(pos_len, rwidth=0.9, align='mid', bins=range(2,20), alpha=0.8, label='Positive Reviews')
+    plt.hist(neg_len, rwidth=0.9, align='mid', bins=range(2,20), alpha=0.8, label='Negative Reviews')
+    plt.title("Number of Stopwords")
+    plt.legend()
+    plt.show()
 
 
 def greedy_searchpara(text_clf, sentiment, tarfname):
@@ -39,20 +69,67 @@ def greedy_searchpara(text_clf, sentiment, tarfname):
     parameters = {
         # 'vect__ngram_range': [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (5, 5)],      # (1, 3) is best
         # 'tfidf__use_idf': [(True, False), (True, True), (False, True), ((False, False))],
-        'clf__C': [2**(i) for i in range(-10, 10)],     # 512 is best
+        'clf__C': [2**(i) for i in range(-10, 15)],     # 512 is best
         # 'clf__class_weight': [None, 'balanced'],  # None is better
         # 'clf__solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],    # 'saga' is better
-        # 'clf__max_iter': [1000^i for i in range(5, 15)],    # iteration 1000
+        # 'clf__max_iter': [10**i for i in range(2, 8)],    # iteration 1000
     }
-    gs_clf = GridSearchCV(text_clf, parameters, cv=5, iid=False, n_jobs=-1, scoring='accuracy', refit=True)
+    from sklearn.metrics import make_scorer
+    from sklearn.metrics import accuracy_score
+    scoring = {'Accuracy': make_scorer(accuracy_score)}
+    gs_clf = GridSearchCV(text_clf, parameters, cv=5, iid=False, n_jobs=-1, scoring=scoring, refit='Accuracy', return_train_score=True)
     gs_clf = gs_clf.fit(sentiment.train_data, sentiment.trainy)
     print(gs_clf.best_score_)
     for param_name in sorted(parameters.keys()):
         print("%s: %r" % (param_name, gs_clf.best_params_[param_name]))
+
+    results = gs_clf.cv_results_
+
+    # plotting the result
+    plt.figure(figsize=(13, 13))
+    # plt.title("GridSearchCV evaluating using multiple scorers simultaneously",
+            # fontsize=16)
+
+    plt.xlabel("the inverse of regularization strength for LogisticRegression Model")
+    plt.ylabel("Score")
+
+    ax = plt.gca()
+
+    # Get the regular numpy array from the MaskedArray
+    X_axis = np.array(results['param_clf__C'].data, dtype=float)
+
+    for scorer, color in zip(sorted(scoring), ['g', 'k']):
+        for sample, style in (('train', '--'), ('test', '-')):
+            sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+            sample_score_std = results['std_%s_%s' % (sample, scorer)]
+            ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+                            sample_score_mean + sample_score_std,
+                            alpha=0.1 if sample == 'test' else 0, color=color)
+            ax.plot(X_axis, sample_score_mean, style, color=color,
+                    alpha=1 if sample == 'test' else 0.7,
+                    label="%s (%s)" % (scorer, sample))
+
+        best_index = np.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
+        best_score = results['mean_test_%s' % scorer][best_index]
+
+        # Plot a dotted vertical line at the best score for that scorer marked by x
+        ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+                linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+
+        # Annotate the best score for that scorer
+        ax.annotate("%0.2f" % best_score,
+                    (X_axis[best_index], best_score + 0.005))
+
+        plt.xscale('log')
+        plt.legend(loc="best")
+        plt.grid(False)
+        plt.show()
     
+    # Evaluate on the refit model
     classify.evaluate(sentiment.train_data, sentiment.trainy, gs_clf, 'train')
     classify.evaluate(sentiment.dev_data, sentiment.devy, gs_clf, 'dev')
 
+    # Evaluate on the unlabeled data
     print("\nReading unlabeled data")
     unlabeled = read_unlabeled(tarfname, sentiment)
     print("Writing predictions to a file")
@@ -94,8 +171,6 @@ def read_files(tarfname):
     sentiment.dev_data, sentiment.dev_labels = read_tsv(tar, devname)
     print(len(sentiment.dev_data))
     # print("-- transforming data and labels")
-    # Data analysis
-    # data_analysis(sentiment)
     sentiment.le = preprocessing.LabelEncoder()
     sentiment.le.fit(sentiment.train_labels)
     sentiment.target_labels = sentiment.le.classes_
@@ -214,12 +289,14 @@ if __name__ == "__main__":
     tarfname = "data/sentiment.tar.gz"
     sentiment = read_files(tarfname)
     print("\nTraining classifier")
-    
+
+    # data_analysis(sentiment)
+
     # Building a pipeline
     text_clf = Pipeline([
-        ('vect', CountVectorizer()),    # ngram_range=(1,3)
-        ('tfidf', TfidfTransformer(use_idf=(True, False))),
-        ('clf', LogisticRegression(random_state=0, solver='saga', max_iter=1000, C=512))   # random_state=0, solver='lbfgs', max_iter=10000
+        ('vect', CountVectorizer(ngram_range=(1,3))),
+        ('tfidf', TfidfTransformer(use_idf=True, smooth_idf=False, sublinear_tf=True)),
+        ('clf', LogisticRegression(random_state=0, C=512, solver='saga', max_iter=1000))
     ])
     text_clf.fit(sentiment.train_data, sentiment.trainy)
     classify.evaluate(sentiment.train_data, sentiment.trainy, text_clf, 'train')
